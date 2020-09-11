bf4e01f6d918e83e66e64f2cfe8ebce331987690
victor.reyes@hotmail.fr <victor.reyes@hotmail.fr>
2020-09-11 12:35:25 +0200

    Friends.js CSS Done

--- src/components/friends/Friends.js
+++ src/components/friends/Friends.js
@@ -1,113 +1,194 @@
-import React, { useEffect, useState } from "react"
-import axios from 'axios'
-import PropTypes from "prop-types"
-import { connect } from 'react-redux'
-import 'react-toastify/dist/ReactToastify.css'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+} from "reactstrap";
 
 const Friends = ({ auth }) => {
-  const [friends, setFriends] = useState(false)
-  const [email, setEmail] = useState('')
+  const [friends, setFriends] = useState(false);
+  const [email, setEmail] = useState("");
 
   useEffect(() => {
     if (auth.isAuthenticated) {
-      console.log('null')
-      axios.get(process.env.REACT_APP_API_LINK + '/friends/list?key=' + process.env.REACT_APP_API_KEY + '&token=' + auth.token)
-        .then(res => {
-          console.log(res.data.users)
-          setFriends(res.data.users)
-        })
+      console.log("null");
+      axios
+        .get(
+          process.env.REACT_APP_API_LINK +
+            "/friends/list?key=" +
+            process.env.REACT_APP_API_KEY +
+            "&token=" +
+            auth.token
+        )
+        .then((res) => {
+          console.log(res.data.users);
+          setFriends(res.data.users);
+        });
+    } else if (auth.isAuthenticated === null) {
+    } else {
+      window.location.replace("http://" + process.env.REACT_APP_LINK);
     }
-    else if (auth.isAuthenticated === null) {
-    }
-    else {
-      window.location.replace('http://' + process.env.REACT_APP_LINK)
-    }
-  }, [auth])
+  }, [auth]);
 
   const updateFriends = () => {
-    axios.get(process.env.REACT_APP_API_LINK + '/friends/list?key=' + process.env.REACT_APP_API_KEY + '&token=' + auth.token)
-      .then(res => {
-        setFriends(res.data.users)
-      })
-  }
+    axios
+      .get(
+        process.env.REACT_APP_API_LINK +
+          "/friends/list?key=" +
+          process.env.REACT_APP_API_KEY +
+          "&token=" +
+          auth.token
+      )
+      .then((res) => {
+        setFriends(res.data.users);
+      });
+  };
 
   const removeFriend = (friendId) => {
-    axios.delete(process.env.REACT_APP_API_LINK + '/friends/friend?key=' + process.env.REACT_APP_API_KEY + '&id=' + friendId + '&token=' + auth.token)
-      .then(res => {
-        updateFriends()
-      })
-  }
+    axios
+      .delete(
+        process.env.REACT_APP_API_LINK +
+          "/friends/friend?key=" +
+          process.env.REACT_APP_API_KEY +
+          "&id=" +
+          friendId +
+          "&token=" +
+          auth.token
+      )
+      .then((res) => {
+        updateFriends();
+      });
+  };
 
   const blockFriend = (friendId) => {
-    axios.post(process.env.REACT_APP_API_LINK + '/friends/block?key=' + process.env.REACT_APP_API_KEY + '&id=' + friendId + '&token=' + auth.token)
-      .then(res => {
-        updateFriends()
-      })
-  }
+    axios
+      .post(
+        process.env.REACT_APP_API_LINK +
+          "/friends/block?key=" +
+          process.env.REACT_APP_API_KEY +
+          "&id=" +
+          friendId +
+          "&token=" +
+          auth.token
+      )
+      .then((res) => {
+        updateFriends();
+      });
+  };
 
   const addFriend = (e) => {
-    e.preventDefault()
-    axios.get(process.env.REACT_APP_API_LINK + '/friends/find?key=' + process.env.REACT_APP_API_KEY + '&emails=' + email + '&token=' + auth.token)
-      .then(res => {
+    e.preventDefault();
+    if (email === "") return false;
+
+    axios
+      .get(
+        process.env.REACT_APP_API_LINK +
+          "/friends/find?key=" +
+          process.env.REACT_APP_API_KEY +
+          "&emails=" +
+          email +
+          "&token=" +
+          auth.token
+      )
+      .then((res) => {
         if (res.data.users.length) {
-          axios.post(process.env.REACT_APP_API_LINK + '/friends/friend?key=' + process.env.REACT_APP_API_KEY + '&token=' + auth.token + '&id=' + res.data.users[0].id)
-            .then(res => {
-              updateFriends()
+          axios
+            .post(
+              process.env.REACT_APP_API_LINK +
+                "/friends/friend?key=" +
+                process.env.REACT_APP_API_KEY +
+                "&token=" +
+                auth.token +
+                "&id=" +
+                res.data.users[0].id
+            )
+            .then((res) => {
+              updateFriends();
             })
-            .catch(error => {
-              toast('There was an error')
-            })
-        }
-        else {
-          toast('This user does not exist')
+            .catch((error) => {
+              toast("Please enter a valid email");
+            });
+        } else {
+          toast("This user does not exist");
         }
       })
-      .catch(error => {
-        toast('There was an error')
-      })
-  }
+      .catch((error) => {
+        toast("Please enter a valid email");
+      });
+  };
 
   return (
     <>
-      <div className="container card p-2">
-        <form onSubmit={addFriend} className='m-2 p-2'>
-          <label>
-            Add friend:
-            <input type="email" name="email" onChange={(e) => setEmail(e.target.value)} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-        {friends ?
-          friends.map((friend, key) => {
-            return (
-              <div key={key} className="card p-2 col-2">
-                <span>{friend.login}</span>
-                <span>{friend.in_account ? <>Online</> : <>Offline</>}</span>
-                <span>Experience points: {friend.xp}</span>
-                <p className="btn btn-success btn-sm m-1" onClick={() => removeFriend(friend.id)}>Remove</p>
-                <p className="btn btn-success btn-sm m-1" onClick={() => blockFriend(friend.id)}>Block</p>
-              </div>
-            )
-          })
-          :
-          null
-        }
+      <Container className="p-5 mt-5 bloc">
+        <h3>My Friends</h3>
+        <Row className="pt-3">
+          <Col sm="6">
+            <Form onSubmit={addFriend}>
+              <FormGroup controlId="exampleForm.ControlInput1">
+                <Label>Add friend:</Label>
+                <Input
+                  type="email"
+                  name="email"
+                  placeholder="Enter email"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <Button className="mt-1">Submit</Button>
+              </FormGroup>
+            </Form>
+          </Col>
+        </Row>
+        <Row>
+          {friends
+            ? friends.map((friend, key) => {
+                return (
+                  <Col key={key} className="card p-3 m-2" sm="3">
+                    <h5>{friend.login}</h5>
+                    <span>
+                      Status : {friend.in_account ? "Online" : "Offline"}
+                    </span>
+                    <span>Experience points : {friend.xp}</span>
+                    <Button
+                      className="btn-sm m-1 mt-3"
+                      onClick={() => removeFriend(friend.id)}
+                      color="secondary"
+                    >
+                      Remove
+                    </Button>
+                    <Button
+                      className="btn-sm m-1"
+                      onClick={() => blockFriend(friend.id)}
+                      color="danger"
+                    >
+                      Block
+                    </Button>
+                  </Col>
+                );
+              })
+            : null}
+        </Row>
         <ToastContainer />
-      </div>
+      </Container>
     </>
-  )
-}
+  );
+};
 
 Friends.propTypes = {
-  auth: PropTypes.object.isRequired
-}
+  auth: PropTypes.object.isRequired,
+};
 
-const mapStateToProps = state => ({
-  auth: state.auth
-})
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
 
-export default connect(
-  mapStateToProps
-)(Friends)
+export default connect(mapStateToProps)(Friends);
